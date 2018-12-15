2d3788e7b7650ff5f943af5a4b87dadc1a87ca46
Kanish <gargkanish23@gmail.com>
2018-12-15 14:29:13 +0530

    added token

--- chat/views.py
+++ chat/views.py
@@ -43,12 +43,12 @@ class MeraBot(generic.View):
         incoming_message = json.loads(self.request.body.decode('utf-8'))
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
+        pprint(incoming_message)
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check to make sure the received call is a message call
                 # This might be delivery, optin, postback for other events 
                 if 'message' in message:
-                    pprint(str(incoming_message))
                     post_facebook_message(message['sender']['id'], message['message']['text']) 
         return HttpResponse()
 
@@ -67,12 +67,15 @@ class Privacy(generic.View):
 
 def post_facebook_message(fbid, recevied_message):
 
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
-    user_details = requests.get(user_details_url, user_details_params).json()
-    joke_text = 'Yo '+str(user_details)+'..!'
+    sent_message="hello"
 
+    send_message(fbid, sent_message)
+    return
+
+
+def send_message(fbid, sent_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+PAGE_ACCESS_TOKEN 
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":sent_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
+    return
